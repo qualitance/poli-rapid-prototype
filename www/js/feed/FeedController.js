d2b9744427defb6b303b59a296e2954bad05197e
cristian stanciulescu <cristian.stanciulescu@qualitance.com>
2015-10-29 18:35:39 +0200

    fb api connect draft

--- www/js/feed/FeedController.js
+++ www/js/feed/FeedController.js
@@ -3,10 +3,18 @@
  */
 
 angular.module('starter.controllers')
-  .controller('FeedController', ['$scope',
-    function($scope) {
+  .controller('FeedController', ['$scope', 'OpenFB',
+    function($scope, OpenFB) {
       $scope.init = function() {
         console.log("IN FEED");
+
+        $scope.data = {};
+
+        OpenFB.get("/me/likes", {fields: 'profile_type,id,name,link,username,category,location'})
+          .success(function(data) {
+            $scope.data.feed = data;
+            console.log(data);
+          });
       };
 
       $scope.init();
